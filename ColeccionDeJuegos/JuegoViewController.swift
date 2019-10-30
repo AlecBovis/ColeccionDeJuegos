8a054bb9ad390bd559b3895ac435323ae65dc6f3
Alec Bovis <AlecBovis@MacBook-Air-de-mbtec22.local>
2019-10-30 11:41:39 -0500

    second commit

--- ColeccionDeJuegos/JuegoViewController.swift
+++ ColeccionDeJuegos/JuegoViewController.swift
@@ -8,23 +8,77 @@
 
 import UIKit
 
-class JuegoViewController: UIViewController {
+class JuegoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var JuegoImagenView: UIImageView!
+    @IBOutlet weak var tituloTextField: UITextField!
+    @IBOutlet weak var agregarActualizarBoton: UIButton!
+    
+    @IBOutlet weak var eliminarBoton: UIButton!
+    
+    
+    var imagePicker = UIImagePickerController()
+    
+    var juego : Juego? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
+        
+        if juego != nil{
+            JuegoImagenView.image = UIImage(data: (juego!.imagen!) as Data)
+            tituloTextField.text = juego!.titulo
+            agregarActualizarBoton.setTitle("Actualizar", for: .normal)
+        }
+        else{
+            eliminarBoton.isHidden = true
+        }
+        
+    }
+    
+    
+    @IBAction func fotosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
 
-        // Do any additional setup after loading the view.
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let imagenSeleccionada = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        JuegoImagenView.image = imagenSeleccionada
+        imagePicker.dismiss(animated: true, completion: nil)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func camaraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
     }
-    */
-
+    
+    
+    @IBAction func agregarTapped(_ sender: Any) {
+       
+        if juego != nil{
+            juego!.titulo = tituloTextField.text
+            juego!.imagen = JuegoImagenView.image!.pngData() as NSData? as Data?
+        }else{
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let juego = Juego(context: context)
+            juego.titulo = tituloTextField.text
+            juego.imagen = JuegoImagenView.image!.pngData() as NSData? as Data?
+        }
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
+    }
+    
+    @IBAction func eliminarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(juego!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
+    }
+    
+    
 }
